d23863ce47cab32bff0db4bd13c11af883eeae6b
DoheumPark <pirbak@hanmail.net>
2024-05-29 15:44:05 +0900

    Test - UserFollowServiceTest

--- src/main/java/com/green/greengram/user/model/UserInfoGetRes.java
+++ src/main/java/com/green/greengram/user/model/UserInfoGetRes.java
@@ -1,10 +1,12 @@
 package com.green.greengram.user.model;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@EqualsAndHashCode
 public class UserInfoGetRes {
     private String nm;
     private String pic;
