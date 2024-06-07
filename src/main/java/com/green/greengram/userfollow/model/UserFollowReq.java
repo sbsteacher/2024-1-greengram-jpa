5e42c110a4526d4b4d19278ca3e82d1054b2b4b9
DoheumPark <pirbak@hanmail.net>
2024-06-07 10:20:28 +0900

    Test - UserFollowController - postUserFollow

--- src/main/java/com/green/greengram/userfollow/model/UserFollowReq.java
+++ src/main/java/com/green/greengram/userfollow/model/UserFollowReq.java
@@ -2,10 +2,12 @@ package com.green.greengram.userfollow.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import java.beans.ConstructorProperties;
 
 @Getter
+@EqualsAndHashCode
 public class UserFollowReq {
     @Schema(name="from_user_id", example = "15", description = "팔로워 유저 아이디", requiredMode = Schema.RequiredMode.REQUIRED)
     private long fromUserId;
---
+++ src/test/java/com/green/greengram/userfollow/UserFollowControllerTest.java
@@ -0,0 +1,65 @@
+package com.green.greengram.userfollow;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.green.greengram.CharEncodingConfiguration;
+import com.green.greengram.common.model.ResultDto;
+import com.green.greengram.userfollow.model.UserFollowReq;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@Import(CharEncodingConfiguration.class)
+@WebMvcTest(UserFollowControllerImpl.class)
+class UserFollowControllerTest {
+    @Autowired private ObjectMapper om;
+    @Autowired private MockMvc mvc;
+    @Autowired private UserFollowController controller;
+    @MockBean private UserFollowServiceImpl service;
+
+    @Test
+    void postUserFollow() throws Exception {
+        UserFollowReq p = new UserFollowReq(1, 2);
+        int resultData = 1;
+        given(service.postUserFollow(p)).willReturn(resultData);
+        String json = om.writeValueAsString(p);
+
+        ResultDto<Integer> expectedResult = ResultDto.<Integer>builder()
+                .statusCode(HttpStatus.OK)
+                .resultMsg(HttpStatus.OK.toString())
+                .resultData(resultData)
+                .build();
+
+        String expectedResultJson = om.writeValueAsString(expectedResult);
+
+        mvc.perform(
+            MockMvcRequestBuilders
+                    .post("/api/user/follow")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(json)
+
+        )
+        .andExpect(status().isOk())
+        .andExpect(content().string(expectedResultJson))
+        .andDo(print());
+
+        verify(service).postUserFollow(p);
+    }
+
+    @Test
+    void deleteUserFollow() {
+    }
+}
